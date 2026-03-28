bddc2520f7f1adc0b1f0049db33df58ebb16f488
miguelon <miguelangel01032001@gmail.com>
2026-03-28 17:19:10 -0500

    Refactor Database connection to use DATABASE_URL

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -7,28 +7,34 @@ use PDOException;
 
 class Database
 {
-    private static $host = 'localhost';
-    private static $db_name = 'startraining';
-    private static $username = 'postgres';
-    private static $password = 'root'; // Use local password
     private static $conn = null;
 
     public static function getConnection()
     {
         if (self::$conn === null) {
             try {
+                $url = getenv("DATABASE_URL");
+
+                if (!$url) {
+                    die("DATABASE_URL no está configurada");
+                }
+
+                $db = parse_url($url);
+
                 self::$conn = new PDO(
-                    "pgsql:host=" . self::$host . ";dbname=" . self::$db_name,
-                    self::$username,
-                    self::$password
-                    );
+                    "pgsql:host={$db['host']};port={$db['port']};dbname=" . ltrim($db['path'], '/'),
+                    $db['user'],
+                    $db['pass']
+                );
+
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            }
-            catch (PDOException $e) {
+
+            } catch (PDOException $e) {
                 die("Connection error: " . $e->getMessage());
             }
         }
+
         return self::$conn;
     }
 }
