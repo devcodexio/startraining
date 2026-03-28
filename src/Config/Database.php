8031e46dabee3843aa3171a05bf0fa1fe8e65777
miguelon <miguelangel01032001@gmail.com>
2026-03-28 17:17:52 -0500

    Refactor Database connection to use static values

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -7,33 +7,28 @@ use PDOException;
 
 class Database
 {
+    private static $host = 'localhost';
+    private static $db_name = 'startraining';
+    private static $username = 'postgres';
+    private static $password = 'root'; // Use local password
     private static $conn = null;
 
     public static function getConnection()
     {
         if (self::$conn === null) {
             try {
-                // 🔹 Variables de entorno (Render)
-                $host = getenv("DB_HOST");
-                $db   = getenv("DB_NAME");
-                $user = getenv("DB_USER");
-                $pass = getenv("DB_PASS");
-                $port = getenv("DB_PORT") ?: 5432;
-
                 self::$conn = new PDO(
-                    "pgsql:host=$host;port=$port;dbname=$db",
-                    $user,
-                    $pass
-                );
-
+                    "pgsql:host=" . self::$host . ";dbname=" . self::$db_name,
+                    self::$username,
+                    self::$password
+                    );
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            } catch (PDOException $e) {
+            }
+            catch (PDOException $e) {
                 die("Connection error: " . $e->getMessage());
             }
         }
-
         return self::$conn;
     }
 }
