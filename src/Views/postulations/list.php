5eeaf909e37eff9efda3f3a3defa7dcb74f81510
miguelon <miguelangel01032001@gmail.com>
2026-03-29 11:29:44 -0500

    Validate CV URL format in list.php
    Validate CV URL before constructing the link.

--- src/Views/postulations/list.php
+++ src/Views/postulations/list.php
@@ -171,7 +171,7 @@ $estadoMap = [
                         $color = matchColor($pct);
                         $estado = $estadoMap[$p['estado_postulacion']] ?? ['label' => $p['estado_postulacion'], 'class' => 'badge-warning'];
                         $isPending = $p['estado_postulacion'] === 'en_espera';
-                        $cvUrl = $p['url_cv_pdf'] ? '/' . $p['url_cv_pdf'] : '';
+                        $cvUrl = $p['url_cv_pdf'] ? (filter_var($p['url_cv_pdf'], FILTER_VALIDATE_URL) ? $p['url_cv_pdf'] : '/' . $p['url_cv_pdf']) : '';
 
                         // Build safe JS strings for the modal
                         $jsDni          = addslashes($p['dni']);
